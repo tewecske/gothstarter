8abf8a324d56bffff8d29ab6107655b88e1c5486
tewe <leventewe@gmail.com>
2024-08-13 01:29:51 +0200

    TODO

---
+++ TODO.lua
@@ -0,0 +1,22 @@
+--[[
+ WARN: remove dummy user
+
+ FIX: Move GothDB to separate file
+
+ TODO: add logout
+ TODO: add google login
+ TODO: add remember me
+ TODO: add a test
+ TODO: add authorization (group from GOTTH)
+ TODO: add proper layout (TailAdmin like)
+ TODO: add design to login form
+ TODO: add register
+ TODO: add dark mode toggle
+
+ HACK:
+
+ NOTE: need easy replace of "gothstarter"
+
+ BUG:
+
+]]
